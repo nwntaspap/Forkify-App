3894aa7d20c99bd05eb3567d8de1fc4d85407a8e
nwntaspap <nwntaspapmichael@gmail.com>
2023-10-05 17:03:52 +0300

    all files

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'be250c00-2af6-447b-8b09-2aeece5d37e3';
+export const MODAL_CLOSE_SEC = 2.5;
